03f91ee138121a35c0fb00a1830ecd9258636fd0
Saulo Filho <saulosilvaf@gmail.com>
2025-02-14 18:14:22 -0300

    fix serialzer of total_balance

--- system/api/app/serializers/store_serializer.rb
+++ system/api/app/serializers/store_serializer.rb
@@ -3,5 +3,9 @@
 class StoreSerializer < Panko::Serializer
   attributes :id, :name, :owner, :total_balance, :created_at, :updated_at
 
+  def total_balance
+    object.transactions.sum(:value)
+  end
+
   has_many :transactions
 end
