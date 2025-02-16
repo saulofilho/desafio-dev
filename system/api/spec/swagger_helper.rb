03d92a3331e568fbdcaf96f2218d21aa160b325d
Saulo Filho <saulosilvaf@gmail.com>
2025-02-16 18:24:52 -0300

    implement config

--- system/api/spec/swagger_helper.rb
+++ system/api/spec/swagger_helper.rb
@@ -3,28 +3,42 @@
 require 'rails_helper'
 
 RSpec.configure do |config|
-  config.swagger_root = Rails.root.join('swagger').to_s
+  config.openapi_root = Rails.root.join(Settings.api.docs.root).to_s
 
-  config.swagger_docs = {
-    'v1/swagger.yaml' => {
-      openapi: '3.0.1',
+  config.openapi_specs = {}
+  Settings.api.docs.versions.each do |api_version|
+    config.openapi_specs[api_version.id] = {
+      openapi: api_version.openapi,
       info: {
-        title: 'API V1',
-        description: 'Documentação da API V1',
-        version: 'v1'
+        title: api_version.title,
+        version: api_version.version
       },
+      paths: {},
       servers: [
         {
-          url: 'http://localhost:3000',
-          description: 'Servidor Local'
-        },
-        {
-          url: 'https://api.seudominio.com',
-          description: 'Servidor de Produção'
+          url: '{defaultHost}',
+          variables: {
+            defaultHost: {
+              default: api_version.default_host
+            }
+          }
         }
-      ]
+      ],
+      components: {
+        schemas: Settings.api.schemas,
+        securitySchemes: {
+          bearer_auth: {
+            type: :http,
+            scheme: :bearer
+          },
+          basic_auth: {
+            type: :http,
+            scheme: :basic
+          }
+        }
+      }
     }
-  }
+  end
 
   config.swagger_format = :yaml
 end
