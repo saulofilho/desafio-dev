5f2cf896ac35123ae7bd54bfd0103e7d77aec25d
Saulo Filho <saulosilvaf@gmail.com>
2025-02-16 18:20:43 -0300

    add users table

---
+++ system/api/db/migrate/20250216195339_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :verification_token
+      t.datetime :verification_token_sent_at
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
--- system/api/db/schema.rb
+++ system/api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_14_133354) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_16_195339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,6 +32,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_14_133354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["store_id"], name: "index_transactions_on_store_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "verification_token"
+    t.datetime "verification_token_sent_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "transactions", "stores"
